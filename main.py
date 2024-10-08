fda3dac5285389c4317b4b5eeb5999853796f39a
KaioMsilva <kaio.contacomercial@gmail.com>
2024-10-08 15:12:47 -0400

    novo botão main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from organizador import organiza_pasta
 from funcoes import mostrar_diretorio
+from localizar_arquivo import localizar
 from tkinter import *
 import tkinter as tk
+
 
 
 # mostra diretorio atual
@@ -20,8 +22,13 @@ website_label.grid(row=2, column=0)
 website_entry = Entry(width=35)
 website_entry.grid(row=2, column=1, columnspan=2)
 website_entry.focus()
-add_button = Button(text="Oraganizar", width=36, command=lambda: organiza_pasta(website_entry))
-add_button.grid(row=4, column=1, columnspan=2)
+
+add_button = Button(text="Oraganizar", width=18, command=lambda: organiza_pasta(website_entry))
+add_button.grid(row=4, column=2, columnspan=1)
+
+add_button = Button(text="localizar", width=18, command=lambda: localizar(website_entry))
+add_button.grid(row=4, column=1, columnspan=1)
+
 
 window.mainloop()
 
